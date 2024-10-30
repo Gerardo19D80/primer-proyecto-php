71f28498e660223bf271133fad04f5669cc7d662
Gerardo19D80 <gdaglio@gmail.com>
2024-10-29 23:29:07 -0300

    agregando archivos al require

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
 require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../public/header.php';
+require __DIR__ . '/../public/head.php';
+require __DIR__ . '/../public/cards.php';
+require __DIR__ . '/../public/styles.css';
 ?>
 
